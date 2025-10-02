6bc510481f62deecb3b040f87e4fb8faec08d46c
Alejandro Cerezo <alce65@hotmail.es>
2025-10-02 11:39:00 +0200

    Relocate config file

--- Web/services/users.ts
+++ Web/services/users.ts
@@ -1,4 +1,4 @@
-import { URL_BASE_API } from '../../config';
+import { URL_BASE_API } from '../config';
 
 export const getUsers = async () => {
   const response = await fetch(`${URL_BASE_API}/users`);
